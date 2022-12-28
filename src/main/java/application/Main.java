211f26154a8275336b722602a46b4e82da05673e
Sirio Junior <sirioroberto1@hotmail.com>
2022-12-27 22:25:32 -0300

    Main view Controller

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -18,7 +18,7 @@ public class Main extends Application {
 
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
-            
+
             Scene mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
